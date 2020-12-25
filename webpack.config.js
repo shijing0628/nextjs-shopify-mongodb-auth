155347a3e6351a16589ca28373a10e72a88238bf
Qixuan Hong <frederickhong.5@gmail.com>
2020-12-25 00:17:12 -0800

    change webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,14 @@
+const path = require('path');
 module.exports = {
   target: 'node',
+  entry: {
+    index: './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index.js',
+    library: 'nextGetShopify',
+    libraryTarget: 'commonjs',
+    globalObject: 'global',
+  },
 };
